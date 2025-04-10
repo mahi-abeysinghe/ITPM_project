96d0b1b40bde791213f7cfc182398bc65d7543c9
Mahi <abeysinghemahi@gmail.com>
2025-04-10 07:44:33 +0530

    create auth types

---
+++ client/src/features/auth/types/index.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: string;
+  name: string;
+  contactNumber: string;
+  role: string;
+  password: string;
+  username: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+}
